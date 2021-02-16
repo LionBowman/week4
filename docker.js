72a3014424d54110b66ab627a7f782503bc275ea
Leandro <bigbruv44@msn.com>
2021-02-16 16:36:38 +0000

    updated to use the correct ip

--- docker.js
+++ docker.js
@@ -2,7 +2,7 @@
 var request = require('request');
 
 //This is the URL endopint of your vm running docker
-var url = 'http://172.16.70.50:2375';
+var url = 'http://192.168.91.30:2375';
 
 
 //this uses the simple get request from request
